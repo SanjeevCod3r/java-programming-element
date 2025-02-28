fb0db4645d8b8f65bbb46890a9d7b6352ad70ee8
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-02-28 21:59:04 +0530

    [Sanjeev Jaiswal] ADD: Calculates the side length of a square based on the perimeter

---
+++ SquareSideCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SquareSideCalculator {
+    public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter the perimeter of the square
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = input.nextDouble();
+
+        // Calculate the side of the square
+        double side = perimeter / 4;
+
+        // Output the result
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+    }
+}
